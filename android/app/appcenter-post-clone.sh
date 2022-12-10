ec4c730788a721bd5b1703d0fe621c39897a0292
arkanalexei <arkanalexei@gmail.com>
2022-12-11 01:09:50 +0700

    inshallah

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,21 +1,36 @@
-#!/usr/bin/env bash
-# place this script in project/android/app/
-cd ..
-# fail if any command fails
-set -e
-# debug log
-set -x
+ #!/usr/bin/env bash
+ # Place this script in project/android/app/
 
-cd ..
-# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
-# stable - recommended for production
+ cd ..
 
-git clone -b stable https://github.com/flutter/flutter.git
-export PATH=`pwd`/flutter/bin:$PATH
+ # fail if any command fails
+ set -e
+ # debug log
+ set -x
 
-flutter channel stable
-flutter doctor
-flutter build apk --release
+ cd ..
+ git clone -b beta https://github.com/flutter/flutter.git
+ export PATH=`pwd`/flutter/bin:$PATH
 
-# copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+ flutter channel stable
+ flutter doctor
+
+ echo "Installed flutter to `pwd`/flutter"
+
+ # export keystore for release
+ echo "$KEY_JKS" | base64 --decode > release-keystore.jks
+
+ # build APK
+ # if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
+ # flutter build apk --debug
+ # flutter build apk --profile
+ flutter build apk --release
+
+ # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
+ #flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
+
+ # copy the APK where AppCenter will find it
+ mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+
+ # copy the AAB where AppCenter will find it
+ #mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
