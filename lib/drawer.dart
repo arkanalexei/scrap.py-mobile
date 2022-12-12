69d12e6ce4eb89f1f6453ab0649ed771060f84e5
arkanalexei <arkanalexei@gmail.com>
2022-12-12 18:34:31 +0700

    tukarpoin

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -6,6 +6,8 @@ import 'package:scrappy/pages/about/aboutUs.dart';
 import 'package:scrappy/pages/about/feedback.dart';
 import 'package:scrappy/pages/about/feedbackform.dart';
 import 'package:scrappy/pages/leaderboard/formComment.dart';
+import 'package:scrappy/pages/tukarpoin/createPerks.dart';
+import 'package:scrappy/pages/tukarpoin/redeem.dart';
 import 'package:scrappy/pages/user/login.dart';
 import 'package:scrappy/pages/user/register.dart';
 import 'package:scrappy/pages/news/newsList.dart';
@@ -189,6 +191,38 @@ class _PublicDrawerState extends State<PublicDrawer> {
                   ),
                 ),
               ],
+            ),
+
+            Visibility(
+              visible: context.watch<UserProvider>().getLogin,
+              child: ExpansionTile(
+                leading: Icon(Icons.article),
+                iconColor: Color(0xFF003320),
+                collapsedIconColor: Color(0xFF003320),
+                title: Text("Tukar Poin"),
+                textColor: Color(0xFF198F85),
+                children: [
+                  ListTile(
+                      title: Text("Perks"),
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Redeem()));
+                      }),
+                  Visibility(
+                    visible: context.watch<UserProvider>().getAdmin,
+                    child: ListTile(
+                        title: Text("Create"),
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const CreatePerks()));
+                        }),
+                  ),
+                ],
+              ),
             ),
 
             ExpansionTile(
